e3744e55264f2b0bcfe37b5eb8eb4fd0f05bbab0
James Cline <james.cline@rd.io>
2013-09-18 19:33:02 -0700

    Ensure multiple-AppDomain safety with minimal speed penalty

--- Gaillard.SharpCover/Counter.cs
+++ Gaillard.SharpCover/Counter.cs
@@ -11,17 +11,35 @@ namespace Gaillard.SharpCover
     {
         private static object mutex = new object();
         private static BinaryWriter writer;
-        private static readonly ISet<int> indexes = new HashSet<int>();
+        private static readonly IDictionary<string,ISet<int>> indexes = new Dictionary<string,ISet<int>>();
+        private static bool registered = false;
+        private static ISet<int> set = null;
 
         public static void Count(string path, int index)
         {
             lock (mutex) {
-                if (writer == null) {
-                    writer = new BinaryWriter(File.Open(path, FileMode.Append));
+                if (!registered) {
+                    AppDomain.CurrentDomain.DomainUnload += CurrentDomain_DomainUnload;
+                    registered = true;
                 }
 
-                if (indexes.Add(index))
-                    writer.Write(index);
+                if (!indexes.TryGetValue(path, out set)) {
+                    indexes.Add(path, new HashSet<int>{index});
+                } else {
+                    set.Add(index);
+                }
+            }
+        }
+
+        private static void CurrentDomain_DomainUnload(object sender, EventArgs e)
+        {
+            lock (mutex) {
+                foreach (var kvp in indexes) {
+                    using (var writer = new BinaryWriter(File.Open(kvp.Key, FileMode.Append))) {
+                        foreach(var i in kvp.Value)
+                            writer.Write(i);
+                    }
+                }
             }
         }
     }
