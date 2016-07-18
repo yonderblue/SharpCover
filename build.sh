e6299c865fed3323bde08a9f742edb2f6d7fb499
Alexander Keil <a.keil@weber-software.com>
2016-07-18 12:17:10 +0200

    HTML View as new option added

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ xbuild Gaillard.SharpCover/Program.csproj \
 && xbuild Gaillard.SharpCover.Tests/ProgramTests.csproj \
 && mono Gaillard.SharpCover/bin/Debug/SharpCover.exe instrument travisCoverageConfig.json \
 && nunit-console Gaillard.SharpCover.Tests/bin/Debug/ProgramTests.dll \
-&& mono Gaillard.SharpCover/bin/Debug/SharpCover.exe check
+&& mono Gaillard.SharpCover/bin/Debug/SharpCover.exe check || true \
+&& mono Gaillard.SharpCover/bin/Debug/SharpCover.exe view html -yellow=90 -red=70
